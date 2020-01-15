d34bf332027668393014bb98e132634a348d01ac
Harun <rashid2k22@gmail.com>
2020-01-15 13:21:48 +0600

    Save user data in mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('./../models/User');
 
@@ -12,7 +13,7 @@ router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please enter a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-], (req, res) => {
+], async (req, res) => {
 
     const errors = validationResult(req);
 
@@ -20,7 +21,33 @@ router.post('/', [
         return res.status(422).json({ errors: errors.array() });
     }
 
-    res.send(req.body);
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists.' });
+        }
+
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('User saved');
+
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
 })
 
 module.exports = router;
