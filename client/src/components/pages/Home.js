c7063eb87786b4525ad717f687b7a230c5840687
Harun <rashid2k22@gmail.com>
2020-01-17 22:29:13 +0600

    Adding contact form

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Contacts from './../contacts/Contacts';
+import ContactForm from './../contacts/ContactForm';
 
 const Home = () => {
     return (
         <div className="grid-2">
-            <Contacts />
+            <div>
+                <ContactForm />
+            </div>
+            <div>
+                <Contacts />
+            </div>
         </div>
     )
 }
