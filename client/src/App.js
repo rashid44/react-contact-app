52e31a155945de60f0e3ecb6a07f164d065bf9d4
Harun <rashid2k22@gmail.com>
2020-01-19 00:42:09 +0600

    Creating context for authentication

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,26 @@ import Navbar from './components/layouts/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 function App() {
   return (
-        <ContactState>
-            <Router>
-                <Fragment>
-                    <Navbar />
+        <AuthState>
+            <ContactState>
+                <Router>
+                    <Fragment>
+                        <Navbar />
 
-                    <div className="container">
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route exact path="/about" component={About} />
-                        </Switch>
-                    </div>
-                </Fragment>
-            </Router>
-        </ContactState>
+                        <div className="container">
+                            <Switch>
+                                <Route exact path="/" component={Home} />
+                                <Route exact path="/about" component={About} />
+                            </Switch>
+                        </div>
+                    </Fragment>
+                </Router>
+            </ContactState>
+        </AuthState>
   );
 }
 
